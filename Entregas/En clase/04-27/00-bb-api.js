1ef8e9cbde6459e315b373d53396a73977ba3ffd
MateoAlzate <brolkwin@gmail.com>
2026-04-27 20:58:49 -0500

    Ejercicio fakeApi

---
+++ Entregas/En clase/04-27/00-bb-api.js
@@ -0,0 +1,9 @@
+const API = "https://fakeapi.net/products";
+
+async function getProducts () {
+    const response = await fetch(API);
+    const data = await response.json();
+    console.log(data);
+}
+
+getProducts();
