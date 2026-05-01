aeb7e3b5ecbfaae938962064df2ccdcc33eae09a
MateoAlzate <brolkwin@gmail.com>
2026-04-30 19:26:00 -0500

    Punto 9 y 10

---
+++ Talleres/Taller-Principiantes/punto9.js
@@ -0,0 +1,7 @@
+let colores = [];
+let color1 = prompt("Ingrese el primer color:");
+let color2 = prompt("Ingrese el segundo color:");
+let color3 = prompt("Ingrese el tercer color:");
+
+colores.push(color1, color2, color3);
+console.log(colores);
