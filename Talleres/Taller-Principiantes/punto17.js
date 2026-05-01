9007ccc9febbb86843ce392021ff3cd2757c389c
MateoAlzate <brolkwin@gmail.com>
2026-04-30 20:03:09 -0500

    punto 17

---
+++ Talleres/Taller-Principiantes/punto17.js
@@ -0,0 +1,10 @@
+let numero = prompt("Ingrese un número:");
+let cero = 0;
+
+if (numero > cero) {
+    console.log( `El número es positivo`)
+}
+
+else if ( numero < cero) { console.log(`el número ingresado es negativo`)}
+
+else { console.log(`El resultado es cero`)};
