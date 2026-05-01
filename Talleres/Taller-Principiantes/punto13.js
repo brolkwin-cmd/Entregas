a4e95654b67e147f72adedc93443d5076700f779
MateoAlzate <brolkwin@gmail.com>
2026-04-30 19:44:29 -0500

    13 y 14

---
+++ Talleres/Taller-Principiantes/punto13.js
@@ -0,0 +1,8 @@
+let persona = {
+    nombre : "jose",
+    edad : 52,
+    ciudad : "Tabogo",
+}
+
+persona.profesion= "Programador";
+console.log(persona);
---
+++ Talleres/Taller-Principiantes/punto14.js
@@ -0,0 +1,10 @@
+let persona = {
+    }
+
+    let nombre = prompt("Ingrese su nombre ");
+    let edad = prompt("Ingrese su edad ");
+
+    persona.nombre = nombre;
+    persona.edad = edad;
+    
+    console.log(persona);
