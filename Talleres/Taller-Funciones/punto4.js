b6a3160922fc81d0e4bfa9428c464d5d0be4a53b
MateoAlzate <brolkwin@gmail.com>
2026-05-07 21:14:41 -0500

    Cuarto Punto

---
+++ Talleres/Taller-Funciones/punto4.js
@@ -0,0 +1,11 @@
+let items = [1,2,3,4,5,6];
+
+function invertirArray(item) {
+    let itemInvertidos = [...item].reverse();
+
+   console.log(item , itemInvertidos);
+}
+invertirArray(items);
+
+
+
